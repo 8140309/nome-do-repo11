d2969fc257c0271bf8bbe02fe67c0ad127120841
Fernando Sousa <8140309@estg.ipp.pt>
2018-10-20 14:06:04 +0100

    Initial commit

--- config.js
+++ config.js
@@ -3,6 +3,8 @@ var config = {};
  config.adminUsername = 'fernando';
  config.adminPassword = '654321aA';
 
+ config.port = 3000;
+
  config.tokenSecretKey = 'super_secret';
  /** Expressed in seconds or a string describing a time span [zeit/ms](https://github.com/zeit/ms.js).  Eg: 60, '2 days', '10h', '7d' */
  config.tokenExireTime = '1h';
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var config = require('./config.js');
  var express = require('express'),
 	 app = express(),
-	 port = process.env.PORT || 3000,
+	 port = process.env.PORT || config.port,
 	 mongoose = require('mongoose'),
 	 bodyParser = require('body-parser');
 
