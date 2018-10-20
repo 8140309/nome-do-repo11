e70aeee9832d08758e17f59f2721568cbe91bf19
Fernando Sousa <8140309@estg.ipp.pt>
2018-10-20 11:46:41 +0100

    Initial commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
- config.adminUsername = 'admin';
- config.adminUsername = '123456';
+ config.adminUsername = 'f';
+ config.adminUsername = 'e';
 
  config.tokenSecretKey = 'super_secret';
  /** Expressed in seconds or a string describing a time span [zeit/ms](https://github.com/zeit/ms.js).  Eg: 60, '2 days', '10h', '7d' */
